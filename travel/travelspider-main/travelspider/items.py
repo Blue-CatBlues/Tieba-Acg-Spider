1c0d43554b5b842309c4b2c621c1aa34a34f2ac8
Blue-CatBlues <vastfireflies@gmail.com>
2024-11-04 20:14:35 +0800

    Delete travel directory

--- travel/travelspider-main/travelspider/items.py
+++
@@ -1,6 +0,0 @@
-import scrapy
-
-class TravelspiderItem(scrapy.Item):
-    name = scrapy.Field()
-    score = scrapy.Field()
-    review_count = scrapy.Field()
--- travel/travelspider-main/travelspider/settings.py
+++
@@ -1,8 +0,0 @@
-SPIDER_MODULES = ['travelspider.spiders']
-NEWSPIDER_MODULE = 'travelspider.spiders'
-
-ROBOTSTXT_OBEY = True
-
-ITEM_PIPELINES = {
-   'travelspider.pipelines.ExcelPipeline': 300,
-}
