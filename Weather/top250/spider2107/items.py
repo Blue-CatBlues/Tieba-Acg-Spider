2940f0fe8c84100319b8d0340fa879c1bb4fa098
Blue-CatBlues <vastfireflies@gmail.com>
2024-11-04 20:13:32 +0800

    Delete Weather directory

--- Weather/top250/spider2107/items.py
+++
@@ -1,6 +0,0 @@
-import scrapy
-
-class MovieItem(scrapy.Item):
-    title = scrapy.Field()
-    rank = scrapy.Field()
-    subject = scrapy.Field()
--- Weather/top250/spider2107/settings.py
+++
@@ -1,8 +0,0 @@
-SPIDER_MODULES = ['spider2107.spiders']
-NEWSPIDER_MODULE = 'spider2107.spiders'
-
-ROBOTSTXT_OBEY = True
-
-ITEM_PIPELINES = {
-   'spider2107.pipelines.ExcelPipeline': 300,
-}
